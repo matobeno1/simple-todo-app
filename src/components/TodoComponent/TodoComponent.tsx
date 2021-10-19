7992bb5aa5368391ad925e29ce1b08024dc54b79
Matej Beno <matobeno1@gmail.com>
2021-10-19 13:14:33 +0200

    Fixing babel dep. + conditional CSS in TodoComponent

--- src/components/TodoComponent/TodoComponent.tsx
+++ src/components/TodoComponent/TodoComponent.tsx
@@ -1,14 +1,26 @@
 import React, { FunctionComponent, PropsWithChildren } from "react";
+import classes from "./TodoComponent.module.css";
+import classNames from "classnames/bind";
 
 export type TodoComponentProps = PropsWithChildren<{
 	isComplete?: boolean;
 }>
 
+const styles = {
+	isComplete: classes.complete
+};
+
+const cx = classNames.bind(styles);
+
 export const TodoComponent: FunctionComponent<TodoComponentProps> = ({
 	isComplete = false,
 }) => {
 	return (
-		<div>
+		<div className={
+			cx({
+				isComplete,
+			})
+		}>
 			{isComplete ? "complete" : "incomplete"}
 		</div>
 	);
