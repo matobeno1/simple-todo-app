9885ba575eacdd32cf39f1b82f547721f5cc297e
Matej Beno <matobeno1@gmail.com>
2021-10-24 20:59:37 +0200

    Changes in styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { FunctionComponent } from "react";
 import { TodosList } from "./components/TodosList";
 import { TodoTitleInput } from "./components/TodoTitleInput";
 import { TodosFilter } from "./components/TodosFilter";
+import { ThemeSwitcherComponent } from "./components/ThemeSwitcherComponent";
 import classes from "./App.module.scss";
 
 const App: FunctionComponent = () => (
 	<div className={classes.root}>
 		<h1>Hello</h1>
+		<ThemeSwitcherComponent />
 		<TodoTitleInput />
 		<TodosList />
 		<TodosFilter />
