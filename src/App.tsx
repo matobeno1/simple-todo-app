e2ee29c31e794b2085e13b1653fdc362bd5bf38f
Matej Beno <matobeno1@gmail.com>
2021-10-24 20:36:40 +0200

    Adding Roboto font.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from "react";
 import { TodosList } from "./components/TodosList";
 import { TodoTitleInput } from "./components/TodoTitleInput";
 import { TodosFilter } from "./components/TodosFilter";
-import classes from "./App.module.css";
+import classes from "./App.module.scss";
 
 const App: FunctionComponent = () => (
 	<div className={classes.root}>
